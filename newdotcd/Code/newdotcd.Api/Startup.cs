4c82c0931f115024017b6db1e8786bce1293d6c0
ShamKarthikS-Hexaware <86096286+ShamKarthikS-Hexaware@users.noreply.github.com>
2022-10-11 15:59:14 +0530

    Update Startup.cs

--- newdotcd/Code/newdotcd.Api/Startup.cs
+++ newdotcd/Code/newdotcd.Api/Startup.cs
@@ -13,7 +13,7 @@ namespace newdotcd.Api
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Configuration = configuration; 
         }
 
         public IConfiguration Configuration { get; }
