5a0758ae24876ff3d5a528e8fdf6ed25ae46d3ae
ShamKarthikS-Hexaware <1000055576@hexaware.com>
2022-10-13 09:08:01 +0000

    dotcheck

---
+++ dotcheck/Code/dotcheck.Business/Installer/ServiceInstaller.cs
@@ -0,0 +1,25 @@
+using dotcheck.Data.Installer;
+using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
+namespace dotcheck.Business.Installer
+{
+    public class ServiceInstaller
+    {
+        private IServiceCollection _service;
+        public ServiceInstaller(IServiceCollection service)
+        {
+            _service = service;
+        }
+
+        public void Install()
+        {
+            _service.Scan(scan => scan
+                                    .FromAssemblyOf<ServiceInstaller>()
+                                    .AddClasses()
+                                    .AsImplementedInterfaces()
+                                    .WithScopedLifetime());
+            var dataInstaller = new DataInstaller(_service);
+            dataInstaller.Install();
+        }
+    }
+}
